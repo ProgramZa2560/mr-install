104c58fbc45370af8d22536de96f3f159e862d46
ProgramZa2560 <ProgramZa2560@users.noreply.github.com>
2026-09-05 06:11:57 +0700

    fix: web build with GSI script (google sign-in on web)

--- app/web/flutter_bootstrap.js
+++ app/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2026432110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1202355747" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
